f51b0de6938cefee3f82e0e11259a386e84a9f62
Lohith <lohith21110257@snuchennai.edu.in>
2026-04-16 17:44:25 -0700

    MLP implementation

--- _MLP/model.py
+++ _MLP/model.py
@@ -1,9 +1,19 @@
 import torch
 import torch.nn as nn
 
-class NameThisAsNeeded(nn.Module):
-    def __init__(self,):
-        pass  # remove this line and add your model's layers here
+class MLP(nn.Module):
+    def __init__(self, layers = [10, 64, 64, 6]):
+        super(MLP, self).__init__()
+
+        self.layers = []
+
+        self.act = nn.ReLU()
+
+        for _ in range(len(layers) - 2):
+            self.layers += [nn.Linear(layers[_], layers[_+1]), self.act]
+        self.layers += [nn.Linear(layers[-2], layers[-1]), nn.Sigmoid()]
+        
+        self.layers = nn.Sequential(*self.layers)
 
     def forward(self, x):
-        pass # remove this line and add the forward pass logic for your model here
+        return self.layers(x)
