3266a815a5b1eb28668f8b26bdcef76f4b0abecf
VSaadeh <victor_saadeh99@yahoo.com>
2026-05-05 00:23:37 -0700

    Successful model architecture, need more training loop and data work

--- _CNN/model.py
+++ _CNN/model.py
@@ -2,32 +2,34 @@ import torch
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def __init__(self, channels_in, num_classes = 10):
+    def __init__(self, channels_in):
         super().__init__()
 
         self.features = nn.Sequential(
 
-            nn.Conv2d(channels_in, 32, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv1d(channels_in, 32, kernel_size = 3, stride = 1, padding = 1),
             nn.ReLU(),
 
-            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.MaxPool1d(kernel_size = 2, stride = 2),
 
-            nn.Conv2d(32, 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv1d(32, 128, kernel_size = 3, stride = 1, padding = 1),
             nn.ReLU(),
 
-            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.MaxPool1d(kernel_size = 2, stride = 2),
 
-            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv1d(128, 256, kernel_size = 3, stride = 1, padding = 1),
             nn.ReLU(),
 
-            nn.MaxPool2d(kernel_size = 2, stride = 2)
+            nn.MaxPool1d(kernel_size = 2, stride = 2)
 
         )
 
         self.flatten = nn.Flatten()
-        self.FullConn_1 = nn.Linear(256 * 3 * 3, 256)
+        self.FullConn_1 = nn.Linear(1499 * 256, 1500)
         self.ReLU_FC = nn.ReLU()
-        self.FullConn_2 = nn.Linear(256, num_classes)
+        self.FullConn_2 = nn.Linear(1500, 256)
+        self.ReLU_FC = nn.ReLU()
+        self.FullConn_3 = nn.Linear(256, 6)
 
     def forward(self, x):
 
@@ -36,6 +38,8 @@ class CNN(nn.Module):
         x = self.FullConn_1(x)
         x = self.ReLU_FC(x)
         x = self.FullConn_2(x)
+        x = self.ReLU_FC(x)
+        x = self.FullConn_3 = nn.Linear(256, 6)
         
         return x
 
@@ -68,3 +72,46 @@ class Net(nn.Module):
         
         return x
 """
+
+# Check the size of each intermediate tensor
+
+Model_Check = CNN(channels_in = 8)
+
+x = torch.randn(800, 8, 11993)
+
+with torch.no_grad():
+  print("Inputs:", x.shape)
+
+  x = Model_Check.features[0](x)
+  x = Model_Check.features[1](x)
+  print("After First Convolution + ReLU:", x.shape)
+
+  x = Model_Check.features[2](x)
+  print("After First Max Pooling:", x.shape)
+
+  x = Model_Check.features[3](x)
+  x = Model_Check.features[4](x)
+  print("After Second Convolution + ReLU:", x.shape)
+
+  x = Model_Check.features[5](x)
+  print("After Second Max Pooling:", x.shape)
+
+  x = Model_Check.features[6](x)
+  x = Model_Check.features[7](x)
+  print("After Third Convolution + ReLU:", x.shape)
+
+  x = Model_Check.features[8](x)
+  print("After Third Max Pooling:", x.shape)
+
+  x = Model_Check.flatten(x)
+  print("After Flattening:", x.shape)
+
+  x = Model_Check.FullConn_1(x)
+  x = Model_Check.ReLU_FC(x)
+  print("After Full Connection 1 + ReLU:", x.shape)
+
+  x = Model_Check.FullConn_2(x)
+  print("After Full Connection 2:", x.shape)
+  
+  x = Model_Check.FullConn_3(x)
+  print("After Full Connection 3:", x.shape)
