1aba2f5320c8f1791c7f585c64551705d224269e
VSaadeh <victor_saadeh99@yahoo.com>
2026-05-10 14:50:57 -0700

    Results produced, finalizing move back from Colab

--- _CNN/model.py
+++ _CNN/model.py
@@ -7,17 +7,17 @@ class CNN(nn.Module):
 
         self.features = nn.Sequential(
 
-            nn.Conv1d(channels_in, 32, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv1d(channels_in, 64, kernel_size = 3, stride = 1, padding = 1),
             nn.ReLU(),
 
             nn.MaxPool1d(kernel_size = 2, stride = 2),
 
-            nn.Conv1d(32, 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv1d(64, 256, kernel_size = 3, stride = 1, padding = 1),
             nn.ReLU(),
 
             nn.MaxPool1d(kernel_size = 2, stride = 2),
 
-            nn.Conv1d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv1d(256, 512, kernel_size = 3, stride = 1, padding = 1),
             nn.ReLU(),
 
             nn.MaxPool1d(kernel_size = 2, stride = 2)
@@ -25,11 +25,11 @@ class CNN(nn.Module):
         )
 
         self.flatten = nn.Flatten()
-        self.FullConn_1 = nn.Linear(1499 * 256, 1500)
+        self.FullConn_1 = nn.Linear(1499 * 512, 1500)
         self.ReLU_FC = nn.ReLU()
-        self.FullConn_2 = nn.Linear(1500, 256)
+        self.FullConn_2 = nn.Linear(1500, 512)
         self.ReLU_FC = nn.ReLU()
-        self.FullConn_3 = nn.Linear(256, 6)
+        self.FullConn_3 = nn.Linear(512, 6)
 
     def forward(self, x):
 
@@ -40,44 +40,14 @@ class CNN(nn.Module):
         x = self.FullConn_2(x)
         x = self.ReLU_FC(x)
         x = self.FullConn_3(x)
-        
+
         return x
-
-# Idan's CNN; checking to see if any difference in results between network architectures
-
-"""
-class Net(nn.Module):
-    def __init__(self,N_channel,kernel_s):
-        super().__init__()
-
-        self.conv1 = nn.Conv2d(N_channel, 32, kernel_size = kernel_s, stride=1, padding=np.ceil((k-1)/2).astype(int))
-        self.pool1 = nn.MaxPool2d(6, 6)
-        self.pool2 = nn.MaxPool2d(3, 3)
-        self.conv2 = nn.Conv2d(32, 128, kernel_size = 3, stride=1, padding=1, device = "cuda")
-        self.conv3 = nn.Conv2d(128, 256, kernel_size = 3, stride=1, padding=1, device = "cuda")
-
-        self.fc1 = nn.Linear(512, 256, device = "cuda")
-        self.fc2 = nn.Linear(256, 2, device = "cuda")
-
-    def forward(self, x):
-        # input image
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = self.pool2(F.relu(self.conv3(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        # 256x1
-        x = self.fc2(x)
-        # 2x1
-        
-        return x
-# """
 
 # # Check the size of each intermediate tensor
 
 # Model_Check = CNN(channels_in = 8)
 
-# x = torch.randn(8, 11993)
+# x = torch.randn(32, 8, 11993)
 
 # with torch.no_grad():
 #   print("Inputs:", x.shape)
